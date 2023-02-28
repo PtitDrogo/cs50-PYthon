bded16c1f3246e2e70859df04be9c2a478137748
Théo Freydière <123643348+PtitDrogo@users.noreply.github.com>
2023-02-28 18:22:30 +0100

    Update professeur.py

--- professeur.py
+++ professeur.py
@@ -1,9 +1,34 @@
 import random
 
+def main():
+    Difficulty = get_level()
+    while True:
+        problem = generate_integer(Difficulty)
+        print(problem)
+
 def get_level():
     while True:
         level_difficulty = int(input("What level, beginner, intermediate or genius ? [1, 2, 3]"))
         if level_difficulty in [1,2,3]:
             return level_difficulty
+        
 
-get_level()
+def generate_integer(level):
+    
+    if level == 1:
+        X = random.randint(1,9)
+        Y = random.randint(1,9)
+        math_problem = X + Y
+        return math_problem
+        
+    elif level == 2:
+        X = random.randint(10,99)
+        Y = random.randint(10,99)
+        math_problem = X + Y
+        return math_problem
+    
+    elif level == 3:
+        X = random.randint(100,999)
+        Y = random.randint(100,999)
+        math_problem = X + Y
+        return math_problem
